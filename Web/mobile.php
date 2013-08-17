50144999ddf03e2754b633e068d7616f3298f690
Pankaj Lele <pankaj@lelesys.com>
2013-08-17 15:11:29 +0530

    [TASK] Add files for mobile demo

---
+++ Web/mobile.php
@@ -0,0 +1,3 @@
+<?php
+echo'<center><div class ="iframe" style="width:500px;height:700px;background-image:url(iphone5-black-preview.jpeg);background-repeat: no-repeat;padding: 100px 0px 0px 26px;"><iframe src="' . "http://" . $_SERVER['HTTP_HOST'] . ' " width="308" height="400" allowtransparency="true" frameborder="0" " style="padding-right: 189px;"></iframe></div></center>';
+?>
